fd1a6b63a08d86e7644ce7dddcac8577d5663217
shehanrandika99 <agsrandika02@gmail.com>
2024-12-25 00:46:41 +0530

    Import NgFor in AppComponent for template iteration support

--- GymApplication/src/app/app.component.ts
+++ GymApplication/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common'; 
-
+import { NgFor } from '@angular/common'; 
 import { CrouselComponent } from './crousel/crousel.component';
 import {TrainersComponent} from './trainers/trainers.component';
 import { ProgramComponent } from './program/program.component';
@@ -12,6 +12,7 @@ import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-root',
   imports: [
+    NgFor,
     RouterOutlet,
     NavBarComponent,
     CommonModule,
@@ -24,9 +25,11 @@ import {MatIconModule} from '@angular/material/icon'
 
     
   ],
+ 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'GymApplication';
+  
 }
